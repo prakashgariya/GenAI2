16748d917e7393f185247dcb191cccdc524dc5bb
prakashgariya <143708519+prakashgariya@users.noreply.github.com>
2023-08-31 21:59:31 +0530

    Add files via upload

--- datepicker.js
+++ datepicker.js
@@ -13,41 +13,31 @@
 
     <script id="oView" name="oView" type="sapui5/xmlview">
     <mvc:View
-	    controllerName="sap.m.sample.DatePicker.Group"
+	    controllerName="myView.Template"
 	    xmlns:mvc="sap.ui.core.mvc"
 	    xmlns:l="sap.ui.layout"
         xmlns:m="sap.m"
 	   >
        <Button id="idButton" press="onButtonPressed" text="Generate Insights"/>
     </mvc:View>
-   </script>   
+   </script>
      `;
  
      class DatePicker extends HTMLElement {
          constructor() {
+            console.log('Inside Constructor');
              super(); 
              _shadowRoot = this.attachShadow({
                  mode: "open"
              });
              _shadowRoot.appendChild(tmpl.content.cloneNode(true));
-
              _shadowRoot.querySelector("#oView").id = _id + "_oView";
-
              this.addEventListener("click", event => {
-               
                  console.log('click');
              });
-
-
-                 
-         }
- 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-         connectedCallback() {
-             loadthis(this);  
          }
 
-         onCustomWidgetAfterUpdate(oChangedProperties) {
+         onCustomWidgetAfterUpdate() {
             loadthis(this);  
         }
          
@@ -72,30 +62,8 @@
             ], function (jQuery, Controller) {
                 "use strict";
 
-                return Controller.extend("sap.m.sample.DatePicker.Group", {
-                    onButtonPressed: function (oEvent) {
-
-                        this.settings = {};
-                        this.settings.date = "";
-
-                        const sToken = "Dummy";
-                        jQuery.ajax({
-                            url: "https://us-central1-us-gcp-ame-con-e74c9-sbx-1.cloudfunctions.net/GCF_Gen_Analytics_trigger",
-                            type: "GET",
-                            crossDomain: true,
-                            beforeSend: function(xhr) {
-                                xhr.setRequestHeader('Authorization', 'Bearer ' + sToken );
-                            },
-                            crossDomain: true,
-                            error: function (err) {
-                                console.log("Error");
-                            },
-                            success: function () {
-                                console.log("Success");
-                            }
-                        });
-                        
-                    }
+                return Controller.extend("myView.Template", {
+                    onButtonPressed: function (oEvent) {}
                 });
             });
 
