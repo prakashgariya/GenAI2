bbed0f33a7c264d1dee097f7c4205010a487a1f2
prakashgariya <143708519+prakashgariya@users.noreply.github.com>
2023-09-01 15:34:56 +0530

    Add files via upload

--- CloudFunction.js
+++ CloudFunction.js
@@ -19,7 +19,7 @@
     </style>
     <div>
     <button class="button">Generate Insights</button>
-    <textarea id="textArea" name="textArea" rows="100" cols="50"></textarea>
+    <textarea id="textArea" name="textArea">Generated Insights will show up here</textarea>
     </div>
     `;
 
@@ -36,6 +36,7 @@
 
         _onButtonClick(event) {
             let sToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM3ZTExNDEwNTlhMTliMjE4MjA5YmM1YWY3YTgxYTcyMGUzOWI1MDAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0Mzc4MzE0ODMyNjg4MzIxNzgwIiwiaGQiOiJkZWxvaXR0ZS5jb20iLCJlbWFpbCI6InVzYS1wZ2FyaXlhQGRlbG9pdHRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiZHFfMXVJamlIbWxQQnRVUGFWSDFrdyIsIm5iZiI6MTY5MzU1MTk2OSwiaWF0IjoxNjkzNTUyMjY5LCJleHAiOjE2OTM1NTU4NjksImp0aSI6ImJlN2ZiMTNiMmU4ZTFlNDg5OTAzZmI0ZTJkZmQzOTRjOTA2NjU1ODEifQ.iKo_bVjn5Y3rgVlWEyGlY83GrON8-lWja-CNfO7LTlMO2IPCcVfnTS671HAAXbjpebvXYkJKi7OPb8mYioHLwgymf-wDQ0TeW9NSjSpPkIdE9_AmW6oFxVnIgETV-lpDC7aolgKwL_1i9xmiE9thpkyNKGv7XE3pJc7sY9dPq3GucBvRjhlflCXeuqOHxi5lv7JuKfwDC4LhupFfDo2OwOlEHU2KzxZNJ7_Fc9VXEMYat689NdQKmKr6N2OWKG_nnnPXbRy_MAW6ZskFjhFUCeiGUxhcnnNbyS4kq0nbpgillTH-WY5BRuTP68wGW0KQc0QJWzdQ9IXM1Ntc_T7q1Q";
+            var textArea = this.shadowRoot.getElementById('textArea');
             jQuery.ajax({
                 url: "https://us-central1-us-gcp-ame-con-e74c9-sbx-1.cloudfunctions.net/GCF_Gen_Analytics_trigger",
                 type: "GET",
@@ -46,11 +47,11 @@
                 crossDomain: true,
                 error: function (err) {
                     console.log("Error");
-                    $("#textArea").val(err);
+                    textArea.value = err.responseText;
                 },
                 success: function (data, textStatus) {
                     console.log("Success");
-                    $("#textArea").val(data);
+                    textArea.value = data;
                 }
             });
         }
