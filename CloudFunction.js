f19038087760f43109f3b603515d80379a35496d
prakashgariya <143708519+prakashgariya@users.noreply.github.com>
2023-09-18 17:36:18 +0530

    Add files via upload

--- CloudFunction.js
+++ CloudFunction.js
@@ -27,57 +27,57 @@
     <textarea id="textArea" name="textArea" class="textarea">click generate insights to get the latest insights provided by your Gen AI agent</textarea>
     </div>
     `;
-//Previous Code
-//Style
-// .button {
-//     background-color: #9ddd58 !important;
-//     border: none;
-//     color: white;
-//     padding: 15px 32px;
-//     text-align: center;
-//     text-decoration: none;
-//     display: inline-block;
-//     font-size: 16px;
-//     margin: 4px 2px;
-//     cursor: pointer;
-//     width:100%;
-// }
-// h2 {
-//     color: white;
-//     border-bottom-style: solid !important;
-//     margin-bottom: unset !important;
-//     margin-top: 0px !important;
-//     background-color: #9ddd58;
-//     border-bottom-color: #9ddd58;
-//     border-top-color: #9ddd58;
-//     height: 30px;
-// }
-// h2{
-//     color: #0088ff;
-//     border-bottom-style: solid !important;
-//     margin-bottom: unset !important;
-//     margin-top: 7px !important;
-// }
-// .img {
-//     display: block;
-//     margin-left: auto;
-//     margin-right: auto;
-//     margin-top: 100px !important;
-// }
-// .img:hover {
-//     transform: scale(0.9) !important;
-// }
-// .bulbIcon{
-//     background: url(https://prakashgariya.github.io/GenAI_Exp/Bulb_Trans.png);
-//     height: auto;
-//     width: auto;
-//     display: block;
-// }
-//HTML
-//<button class="button">Generate Insights</button>
-//<img id="image" class="img" src="https://prakashgariya.github.io/GenAI_Exp/VishalKrish.png" alt="Finance Analyst" width="200px" height="450px"/>
-//<h2><i class="bulbIcon"></i>&nbsp&nbsp&nbsp&nbspInsights</h2>
-//<img id="image" class="img" src="https://prakashgariya.github.io/GenAI_Exp/VishalKrish.png" alt="Finance Analyst" width="150px" height="175px"/>
+    //Previous Code
+    //Style
+    // .button {
+    //     background-color: #9ddd58 !important;
+    //     border: none;
+    //     color: white;
+    //     padding: 15px 32px;
+    //     text-align: center;
+    //     text-decoration: none;
+    //     display: inline-block;
+    //     font-size: 16px;
+    //     margin: 4px 2px;
+    //     cursor: pointer;
+    //     width:100%;
+    // }
+    // h2 {
+    //     color: white;
+    //     border-bottom-style: solid !important;
+    //     margin-bottom: unset !important;
+    //     margin-top: 0px !important;
+    //     background-color: #9ddd58;
+    //     border-bottom-color: #9ddd58;
+    //     border-top-color: #9ddd58;
+    //     height: 30px;
+    // }
+    // h2{
+    //     color: #0088ff;
+    //     border-bottom-style: solid !important;
+    //     margin-bottom: unset !important;
+    //     margin-top: 7px !important;
+    // }
+    // .img {
+    //     display: block;
+    //     margin-left: auto;
+    //     margin-right: auto;
+    //     margin-top: 100px !important;
+    // }
+    // .img:hover {
+    //     transform: scale(0.9) !important;
+    // }
+    // .bulbIcon{
+    //     background: url(https://prakashgariya.github.io/GenAI_Exp/Bulb_Trans.png);
+    //     height: auto;
+    //     width: auto;
+    //     display: block;
+    // }
+    //HTML
+    //<button class="button">Generate Insights</button>
+    //<img id="image" class="img" src="https://prakashgariya.github.io/GenAI_Exp/VishalKrish.png" alt="Finance Analyst" width="200px" height="450px"/>
+    //<h2><i class="bulbIcon"></i>&nbsp&nbsp&nbsp&nbspInsights</h2>
+    //<img id="image" class="img" src="https://prakashgariya.github.io/GenAI_Exp/VishalKrish.png" alt="Finance Analyst" width="150px" height="175px"/>
 
     class CloudFunction extends HTMLElement {
         constructor() {
@@ -94,7 +94,7 @@
         }
 
         _onButtonClick(event) {
-            let sToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjgzOGMwNmM2MjA0NmMyZDk0OGFmZmUxMzdkZDUzMTAxMjlmNGQ1ZDEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0Mzc4MzE0ODMyNjg4MzIxNzgwIiwiaGQiOiJkZWxvaXR0ZS5jb20iLCJlbWFpbCI6InVzYS1wZ2FyaXlhQGRlbG9pdHRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiVmZ0RDJBYmxZZFdNS1ZxRzZpYzAzdyIsIm5iZiI6MTY5NDUwNzU1OCwiaWF0IjoxNjk0NTA3ODU4LCJleHAiOjE2OTQ1MTE0NTgsImp0aSI6IjQ1NmNlMDliNDgzMzQxZGVjMWE0YTA1NjdkNmI0NWY5NzUzNjk1NDYifQ.IAxPDcxuGF51mVJyrS7PBXok4L9Kq4NPdAPnLtZwvvTAtN-8K8F-7nECRhUmYrA0_3y10R8U7ohkAhTy391mJaWVTP0MBM-U5AzlYkV8xOs5JmHy65ljfOfaAG5fXbleAyT4h2YVWW9w5-kHknqfdWYCt0VYvH9iFU6MnIKLqq8f0ZbknTWMufBbASNe9LSPnV_YbLgqVkNpTuoI6_OmUtoMgtVwNzEa02LPJgWoOSs88pgFcfhy_km4pQAMrBRUOP5B5IAegED11sNEMWGutfjrjPaIecHhB4XWOK5ijr61X22mR28zec3B-z0mT5Gd36zDZeLbcyc6r6_6DMIXT";
+            let sToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjdjMGI2OTEzZmUxMzgyMGEzMzMzOTlhY2U0MjZlNzA1MzVhOWEwYmYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0Mzc4MzE0ODMyNjg4MzIxNzgwIiwiaGQiOiJkZWxvaXR0ZS5jb20iLCJlbWFpbCI6InVzYS1wZ2FyaXlhQGRlbG9pdHRlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiNmJ2SFl2M1RuMjVaMmRFWWdBM1FZZyIsIm5iZiI6MTY5NTAzODQxMywiaWF0IjoxNjk1MDM4NzEzLCJleHAiOjE2OTUwNDIzMTMsImp0aSI6ImVmYjg4ZDQxMmY5M2Q2ZmFkNjk2MjMyNzYwNDI2MTk5NmU2Yzg3ZTIifQ.AH2JD9Gm4GglWfqXonLhj3PFzxD9RxpOpUHr-v9qsGhtX-mOvfleFAt65Xd4-yb-6p6LdjnvAcONEMcm-8_jc8H0xecOZ5lkHtA4hspYn1T58GreFFWP9zcZBl3fecoFPeuZVzo0--OyrjFYS9NYhlSjpi36RkerR3UpN5dW6UFEbL-BDt5-BXbqpChdL07aoOiweElY9bAfujJXizrZiXLiL0J86QNVo4EFTZQe3VpE3JxNXfk7qAYjhw1hOohOQexr0FivnJAl46WIsdZyyBvBmiXIsdmiPQwoCjMrUUlWj5HcvwQRnAn7CfF3FuUsmUKslr6j0SA3_0GbJmWqCA";
             var textArea = this.shadowRoot.getElementById('textArea');
             textArea.value = "Fetching Insights from Open AI LLM......";
             //var image = this.shadowRoot.getElementById('image');
@@ -102,6 +102,9 @@
             // this._shadowRoot.querySelector(".img").style.display = "None";
             //image.style.display = "None";
             //textArea.hidden = false;
+
+            //Typewriter display via JS code
+            var i=0, txt = "",speed = 50;
             jQuery.ajax({
                 url: "https://us-central1-us-gcp-ame-con-e74c9-sbx-1.cloudfunctions.net/GCF_Gen_Analytics_trigger",
                 type: "GET",
@@ -112,7 +115,16 @@
                 crossDomain: true,
                 error: function (err) {
                     console.log("Error");
-                    textArea.value = err.responseText;
+                    // textArea.value = err.responseText;
+                    txt = err.responseText;
+                    _setTypeWriterEffect();
+                    function _setTypeWriterEffect(){
+                        if(i < txt.length){
+                            textArea.value += txt.charAt(i);
+                            i++;
+                            setTimeout(_setTypeWriterEffect, speed);
+                        }
+                    }
                 },
                 success: function (data, textStatus) {
                     console.log("Success");
